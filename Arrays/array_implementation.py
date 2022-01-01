afa3cac55725daf5b2e9dddde43b7c62feacdac5
smane <37468927+sanketmane@users.noreply.github.com>
2022-01-01 19:58:25 +0000

    array implementation

---
+++ Arrays/array_implementation.py
@@ -0,0 +1,41 @@
+# Array implementation
+
+class MyArray:
+  def __init__(self):
+    self.length = 0
+    self.data = {}
+
+  def get(self,index):
+    return self.data[index]
+
+  def push(self,item):
+    self.data[self.length] = item
+    self.length += 1
+    return self.data
+
+  def pop(self):
+    del self.data[self.length - 1]
+    self.length -= 1
+    return self.data
+
+  
+
+  def delete(self,index):
+    for i in range(index,self.length-1):
+      self.data[i] = self.data[i+1] 
+      # there is no value at i+1 at the last index
+
+    del self.data[self.length-1]
+    self.length -= 1
+    return self.data
+
+  
+
+arr = MyArray()
+arr.push('hi')
+arr.push('are')
+arr.push('you')
+arr.push('there')
+arr.delete(1)
+print(arr.data)
+
